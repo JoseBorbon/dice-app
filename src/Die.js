c3b137c813bcefeb281f00b554de6d4efa2c63f8
JoseBorbon <joserborbon@gmail.com>
2021-10-15 06:24:17 -0400

    added className for dice shaking animation

--- src/Die.js
+++ src/Die.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class Die extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     const input = `fas fa-dice-${this.props.diceNumber} fa-5x`;
     return (
       <div>
-        <i class={input}></i>
+        <i className={`${input} ${this.props.className}`}></i>
       </div>
     );
   }
